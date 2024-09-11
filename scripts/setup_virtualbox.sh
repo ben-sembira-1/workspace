96d66c957702168d74d1dc89c6f312411b9676b7
Ben <ben.sembira.dev@gmail.com>
2024-09-11 15:15:13 +0300

    small, important comment

--- scripts/setup_virtualbox.sh
+++ scripts/setup_virtualbox.sh
@@ -5,4 +5,5 @@ set -euo pipefail
 sudo apt-get install -y virtualbox virtualbox-ext-pack
 # At first stage - press enter multiple times and then yes.
 # Then again but instead of yes, create a new password.
+# Make sure to disable secure boot.
 
