3d8c7f1a0ad337f963ddd7f7939f132a29147d6c
Ben <ben.sembira.dev@gmail.com>
2024-09-11 10:03:47 +0300

    fix: make scripts executable

--- scripts/setup_homebrew.sh
+++ scripts/setup_homebrew.sh
@@ -5,5 +5,5 @@ set -euo pipefail
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"') >> ~/.bashrc
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-sudo apt-get install build-essential
+sudo apt-get install -y build-essential
 brew install gcc
