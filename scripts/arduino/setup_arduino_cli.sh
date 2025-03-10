13972082e288fa929f1a2e62430cdd3e3c8d0ab8
Ben <ben.sembira.dev@gmail.com>
2025-03-10 19:30:14 +0200

    feat: arduino-cli init

--- scripts/arduino/setup_arduino_cli.sh
+++ scripts/arduino/setup_arduino_cli.sh
@@ -4,3 +4,8 @@ set -euo pipefail
 
 brew update
 brew install arduino-cli
+
+arduino-cli config init
+arduino-cli core update-index
+arduino-cli core install arduino:avr
+arduino-cli lib install "AUnit@1.7.1"
